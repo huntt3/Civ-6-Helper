2563c789eb0b50f30e589f62bce41d88a8d6470f
Trevor Hunt <157053920+huntt3@users.noreply.github.com>
2025-07-03 05:28:31 +0000

    Commit 5. Added Tech Tree Carousels.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import DistrictCards from "./DistrictCards";
+import TreeCarousel from "./TreeCarousel";
 
 // Main App component
 function App() {
   return (
     <>
       <header className="App-header">
-        <h1>
-          Civ 6 Helper
-        </h1>
+        <h1>Civ 6 Helper</h1>
       </header>
-      {/* Render the DistrictCards component below the header */}
+      {/* Render the DistrictCards component above the carousels */}
       <DistrictCards />
+      {/* Render two TreeCarousels below the District Cards */}
+      <TreeCarousel />
+      <TreeCarousel />
     </>
   );
 }
