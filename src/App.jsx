3b7967e208fcccff21e8bf32e565e10a7eaf1936
Trevor Hunt <157053920+huntt3@users.noreply.github.com>
2025-07-06 01:10:52 +0000

    Commit 25. Manifest and other changes to remove all errors.

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,10 @@ function App() {
       <header className="App-header">
         <h1>Civ 6 Helper</h1>
       </header>
-      {/* Render the DistrictCards component above the carousels */}
       <DistrictCards />
-      {/* Render TechCarousel below DistrictCards */}
       <TechCarousel />
-      {/* Render EraTrackerContainer below the District Cards */}
       <EraTrackerContainer />
-      {/* Render WondersContainer at the bottom */}
       <WondersContainer />
-      {/* Render GreatPeopleContainer at the very bottom */}
       <GreatPeopleContainer />
     </main>
   );
