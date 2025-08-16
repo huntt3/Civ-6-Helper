8728700778afeb4b5c00dcb307dd8eb3996e9438
Trevor <huntt3@mail.uc.edu>
2025-08-16 16:33:07 -0400

    Deploy error bugfixing

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/Civ-6-Helper/", // Set base path for GitHub Pages deployment
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        districtDiscountingTool: resolve(
+          __dirname,
+          "districtDiscountingTool.html"
+        ),
+        eraScoreTracker: resolve(__dirname, "eraScoreTracker.html"),
+        greatPeopleTracker: resolve(__dirname, "greatPeopleTracker.html"),
+        hexPlanner: resolve(__dirname, "hexPlanner.html"),
+        techsAndCivicsTree: resolve(__dirname, "techsAndCivicsTree.html"),
+        wonderTracker: resolve(__dirname, "wonderTracker.html"),
+      },
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
