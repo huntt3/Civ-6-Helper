6d7b9817321ca894acbbc066cfec0f9ca11fc20b
Trevor <huntt3@mail.uc.edu>
2025-08-16 15:33:12 -0400

    District Discounting Explanation styling

--- src/DistrictDiscountingTool/DistrictDiscountingExplanation.jsx
+++ src/DistrictDiscountingTool/DistrictDiscountingExplanation.jsx
@@ -4,26 +4,47 @@ const DistrictDiscountingExplanation = ({
   numSpecialtyDistrictsCompleted,
   numSpecialtyDistrictsUnlocked,
 }) => (
-  <>
-    <div>
-      Districts are discounted when both of the following conditions are met:
+  <div className="p-4 bg-blue-50 rounded-xl shadow-md border border-blue-200">
+    <div className="mb-4 text-lg font-semibold text-blue-900">
+      Districts are discounted when <span className="underline">both</span> of
+      the following conditions are met:
     </div>
-    <div>
-      The number of specialty districts completed &gt;= The number of specialty
-      districts unlocked
-    </div>
-    <div>
-      The number of districts of the type that have been completed or placed
-      &lt; (The number of specialty districts completed / The number of
-      specialty districts unlocked)
-    </div>
+    <ul className="list-disc pl-6 space-y-4">
+      <li>
+        <div className="flex flex-row items-center gap-2 text-xl">
+          <span className="font-mono font-bold text-blue-900">
+            Specialty Districts Completed
+          </span>
+          <span className="mx-2 text-2xl font-bold text-blue-700">&#8805;</span>
+          <span className="font-mono font-bold text-blue-900">
+            Specialty Districts Unlocked
+          </span>
+        </div>
+      </li>
+      <li>
+        <div className="flex flex-row items-center gap-2 text-xl">
+          <span className="font-mono font-bold text-blue-900">
+            Districts of Type Completed or Placed
+          </span>
+          <span className="mx-2 text-2xl font-bold text-blue-700">&lt;</span>
+          <span className="flex flex-col items-center">
+            <span className="font-mono font-bold text-blue-900 border-b-2 border-blue-700 px-2 pb-1">
+              Specialty Districts Completed
+            </span>
+            <span className="font-mono font-bold text-blue-900 px-2 pt-1">
+              Specialty Districts Unlocked
+            </span>
+          </span>
+        </div>
+      </li>
+    </ul>
     <div className="mt-4 font-bold text-blue-700">
       Total Specialty Districts Completed: {numSpecialtyDistrictsCompleted}
     </div>
     <div className="mb-2 font-bold text-blue-700">
       Total Specialty Districts Unlocked: {numSpecialtyDistrictsUnlocked}
     </div>
-  </>
+  </div>
 );
 
 export default DistrictDiscountingExplanation;
